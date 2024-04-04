b465f652aa8857186cba6d24cdc57f8189aee0d5
Marcos Farias <marcosmiguelfarias@gmail.com>
2024-04-04 16:42:33 +0200

    rework the login page
    use forms
    use style in same file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,79 @@
 'use client';
 
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { Tabs, Input, Flex, Checkbox, Typography } from 'antd';
-import type { TabsProps } from 'antd';
+import styled from '@emotion/styled';
+import { Tabs, Input, Flex, Checkbox, Typography, Button, Form } from 'antd';
+import type { FormProps, TabsProps } from 'antd';
+import { smallAndSmaller } from 'app/app.layout';
 import { handleLogin, withoutAuthentication } from 'libs/authentication';
 import React, { useState } from 'react';
-import {
-    Terms,
-    EnterButton,
-    Container,
-    Form,
-    input_icon,
-    input_height,
-    LoginErrorContainer,
-    ImageLogo,
-} from './login.styled';
+
+const Container = styled.div((props) => ({
+    alignItems: 'center',
+    display: 'flex',
+    [smallAndSmaller]: {
+        flexDirection: 'column',
+        gap: 0,
+    },
+    gap: '12rem',
+    justifyContent: 'center',
+}));
+
+const MainForm = styled(Form)((props) => ({
+    span: {
+        color: 'var(--font-primary)',
+    },
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.75rem',
+    width: '22.5rem',
+}));
+
+const input_height = '3.125rem';
+
+const input_icon = {
+    color: 'var(--secondary)',
+};
+
+const ImageLogo = styled.div((props) => ({
+    [smallAndSmaller]: {
+        backgroundImage: "url('/mettle-logo.svg')",
+        width: '7.92rem',
+        height: '2.1rem',
+        marginBottom: '1.25rem',
+        marginTop: '1.25rem',
+    },
+    backgroundImage: "url('/mettle-login.svg')",
+    width: '42.6875rem',
+    height: '43rem',
+}));
+
+const EnterButton = styled(Button)(() => ({
+    height: '3.125rem',
+    span: {
+        color: 'var(--primary)',
+    },
+}));
+
+const Terms = styled.div((props) => ({
+    span: {
+        fontSize: '0.6rem',
+        width: '20rem',
+    },
+    'span > a': {
+        fontSize: '0.6rem',
+    },
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 0,
+}));
+
+const LoginErrorContainer = styled.div((props) => ({
+    color: 'var(--danger)',
+    fontSize: '0.8rem',
+    paddingBottom: '0.4rem',
+}));
 
 function Page() {
     const items: TabsProps['items'] = [
@@ -47,65 +106,67 @@ function Page() {
 }
 
 function LoginForm() {
-    const [formEmail, setFormEmail] = useState<string>('');
-    const [formPassword, setFromPassword] = useState<string>('');
     const [loginErrorMessage, setLoginErrorMessage] = useState<string | null>(null);
     const [isSignInLoading, setIsSignInLoading] = useState<boolean>(false);
 
-    const handleSubmit = async () => {
-        console.log(formEmail);
-        console.log(formPassword);
+    type FieldType = {
+        email?: string;
+        password?: string;
+        remember?: string;
+    };
+
+    const onFinish: FormProps['onFinish'] = async (values: FieldType) => {
         await handleLogin({
-            email: formEmail as string,
-            password: formPassword as string,
+            email: values.email!,
+            password: values.password!,
             setLoginErrorMessage,
             setIsSignInLoading,
         });
     };
 
+    const requiredRule = { required: true, message: 'Campo obrigatório' };
+
     return (
-        <Form>
+        <MainForm name="login" onFinish={onFinish} autoComplete="off">
             <Flex vertical>
                 <Typography.Text>Seu e-mail</Typography.Text>
-                <Input
-                    size="large"
-                    style={{ height: input_height }}
-                    placeholder="exemplo@email.com.br"
-                    prefix={<UserOutlined style={input_icon} />}
-                    onChange={(event) => {
-                        console.log(event);
-                        setFormEmail(event.target.value);
-                        setLoginErrorMessage(null);
-                    }}
-                />
+                <Form.Item name="email" rules={[{ type: 'email', message: 'E-mail inválido' }, ...[requiredRule]]}>
+                    <Input
+                        size="large"
+                        style={{ height: input_height }}
+                        placeholder="exemplo@email.com.br"
+                        prefix={<UserOutlined style={input_icon} />}
+                        onChange={(event) => {
+                            setLoginErrorMessage(null);
+                        }}
+                    />
+                </Form.Item>
             </Flex>
             <Flex vertical>
                 <Typography.Text>Sua senha</Typography.Text>
-                <Input
-                    size="large"
-                    style={{ height: input_height }}
-                    placeholder="senha"
-                    prefix={<LockOutlined style={input_icon} />}
-                    type="password"
-                    onChange={(event) => {
-                        setFromPassword(event.target.value);
-                        setLoginErrorMessage(null);
-                    }}
-                />
+                <Form.Item name="password" rules={[requiredRule]}>
+                    <Input
+                        size="large"
+                        style={{ height: input_height }}
+                        placeholder="senha"
+                        prefix={<LockOutlined style={input_icon} />}
+                        type="password"
+                        onChange={(event) => {
+                            setLoginErrorMessage(null);
+                        }}
+                    />
+                </Form.Item>
                 {loginErrorMessage && <LoginErrorContainer>{loginErrorMessage}</LoginErrorContainer>}
                 <Flex justify="space-between">
-                    <Checkbox>
-                        <Typography.Text>Salvar meus dados</Typography.Text>
-                    </Checkbox>
+                    <Form.Item name="remember">
+                        <Checkbox>
+                            <Typography.Text>Salvar meus dados</Typography.Text>
+                        </Checkbox>
+                    </Form.Item>
                     <Typography.Link href="/forgot-password">Esqueceu a senha?</Typography.Link>
                 </Flex>
             </Flex>
-            <EnterButton
-                type="primary"
-                onClick={async () => {
-                    await handleSubmit();
-                }}
-            >
+            <EnterButton type="primary" htmlType="submit">
                 Entrar
             </EnterButton>
             <Terms>
@@ -117,7 +178,7 @@ function LoginForm() {
                     </Typography.Link>
                 </Typography.Text>
             </Terms>
-        </Form>
+        </MainForm>
     );
 }
 

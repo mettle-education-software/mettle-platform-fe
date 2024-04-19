71763c5216993b5fea2dd046a5ab30c53ae87be6
Pedro Filipe Fontes <pedrofilipe.silvafontes@gmail.com>
2024-04-19 20:47:43 +0200

    Add page layout

--- src/app/melp/community/page.tsx
+++ src/app/melp/community/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
+import { AppLayout } from 'components';
 import React from 'react';
 
 export default function CommunityPage() {
     return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <iframe
-                style={{ width: '100%', height: '100%' }}
-                src="https://pedros-community-da05a9.circle.so/c/melp-users?iframe=true"
-            />
-        </div>
+        <AppLayout withMelpSummary>
+            <div style={{ width: '100%', height: '100vh' }}>
+                <iframe
+                    style={{ width: '100%', height: '100%' }}
+                    src="https://pedros-community-da05a9.circle.so/c/melp-users?iframe=true"
+                />
+            </div>
+        </AppLayout>
     );
 }

f37995a93833d333fc3282f4f380ae0f82412886
Pedro Filipe Fontes <pedrofilipe.silvafontes@gmail.com>
2024-04-19 20:57:56 +0200

    add authentication

--- src/app/melp/community/page.tsx
+++ src/app/melp/community/page.tsx
@@ -9,7 +9,7 @@ function CommunityPage() {
         <AppLayout withMelpSummary>
             <div style={{ width: '100%', height: '100vh' }}>
                 <iframe
-                    style={{ width: '100%', height: '100%' }}
+                    style={{ width: '100%', height: '100%', border: 'none' }}
                     src="https://pedros-community-da05a9.circle.so/c/melp-users?iframe=true"
                 />
             </div>

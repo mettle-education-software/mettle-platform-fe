4bfef818ad253d5c970dcf885a156006a8594b40
Pedro Filipe Fontes <pedrofilipe.silvafontes@gmail.com>
2025-05-26 21:02:06 +0200

    GTM fix attempt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,10 +45,7 @@ const App = ({ children }: { children: React.ReactNode }) => {
                         src="https://www.googletagmanager.com/ns.html?id=GTM-KQF8NQS8"
                         height="0"
                         width="0"
-                        style={{
-                            display: 'none',
-                            visibility: 'hidden',
-                        }}
+                        style={{ display: 'none', visibility: 'hidden' }}
                     ></iframe>
                 </noscript>
                 {/** <!-- End Google Tag Manager (noscript) --> **/}
@@ -100,7 +97,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KQF8NQS8');
+                    })(window,document,'script','dataLayer','GTM-KQF8NQS8');
                 `}</script>
                 {/*<!-- End Google Tag Manager -->*/}
                 <title>Plataforma Mettle</title>
